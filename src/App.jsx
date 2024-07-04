67df5642b2235a31af2103766b0625297df93522
gauravsingh906 <gauravsingh8xm@gmail.com>
2024-07-04 21:47:44 +0530

    added link adding pokedex to poke-dex

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,13 @@ import CustomRoutes from './routes/CustomRoutes'
 import { Link } from 'react-router-dom'
 
 function App() {
- 
+
 
   return (
     <div className='outer-pokedex'>
-    {/* //common content can be written as simple */}
-    <h1 id="pokedex-heading">
-      <Link to='/'>Pokedex</Link></h1>
+      {/* //common content can be written as simple */}
+      <h1 id="pokedex-heading">
+        <Link to='/'>Poke-dex</Link></h1>
       <CustomRoutes />
 
     </div>
